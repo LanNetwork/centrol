732beec0ff8c7aea9b0a669691f7c768cf51ce41
LanNetwork <ianmcgeoy@gmail.com>
2023-06-08 16:30:46 -0700

    Deleted old function, changed comment

--- internal/article/article.go
+++ internal/article/article.go
@@ -15,7 +15,7 @@ type AtomArticle struct {
 // https://feeds.simplecast.com/54nAGcIl for example, is a hybrid document that uses RSS 2.0, and atom.
 // Atom elements in the document are labeled with "itunes: whateverfield", because itunes also uses atom 1.0
 //
-// Also, "media:whateverfield" is a different format. It might be called mrss...
+// Also, "media:whateverfield" is a different format. It's a namespace, might be called mrss...
 
 // This Object also has metadata mappings for json, to allow for marshalling into json.
 type RSSFeed struct {
@@ -32,21 +32,8 @@ type Item struct {
 	Title string `xml:"title"`
 	// xml Descriptions may be CDATA, which requires parsing out the ctext tag using xml.unmarshal()
 	Description string `xml:"description"`
-	// content:encoded isn't parsing at all. Why? I think it's just a limitation of Go... Spaces and such.
+	// content:encoded isn't parsing at all. Why? I think it's just a limitation of Go... Namespaces and such.
 	// ContentEncoded CDATA  `xml:"content\\:encoded"`
 	PubDate string `xml:"pubDate"`
 	Link    string `xml:"link"`
 }
-
-// Function for trying to parse content:encoded. Didn't work :(
-// type CDATA string
-
-// func (c *CDATA) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-// 	var content string
-// 	err := d.DecodeElement(&content, &start)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	*c = CDATA(content)
-// 	return nil
-// }
